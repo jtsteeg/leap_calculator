041f1818ac545bebf11e01c6e5abb6679989fc8c
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-11-08 13:03:38 -0600

    adds event listeners to number input buttons

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 (()=>{
+    //elements
+    var numbers = document.getElementsByClassName("numberButton");
+    var numberDisplay = document.getElementById("numDisplay");
 
+    console.log(numbers);
+    console.log(numberDisplay);
 
+    //classes
+    class Calculator{
+        constructor(){
+            this.tempDisplay = 0;
+        }
+
+        newNumberInput(number) {
+            this.tempDisplay = this.tempDisplay * 10 + parseInt(number);
+            this.showOnDisplay();
+        }
+
+        showOnDisplay() {
+            numberDisplay.value = this.tempDisplay;
+            console.log(this.tempDisplay);
+        }
+    }
+
+    var calc = new Calculator();
+
+    //eventlisteners
+    for(var i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', function() {
+            calc.newNumberInput(this.innerText);
+        });
+
+    }
 
 
 })()
